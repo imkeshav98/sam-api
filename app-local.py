48d9785a06231ee98e508848951954efc8c075d3
Keshav <keshav.chaudhary@deltax.com>
2024-11-28 03:58:21 +0530

    fine tune values

--- app-local.py
+++ app-local.py
@@ -29,7 +29,19 @@ MODEL_CFG = "configs/sam2.1/sam2.1_hiera_l.yaml"
 
 
 sam2 = build_sam2(MODEL_CFG, SAM2_CHECKPOINT, device=device, apply_postprocessing=True)
-mask_generator = SAM2AutomaticMaskGenerator(sam2)
+mask_generator = SAM2AutomaticMaskGenerator(
+    model=sam2,
+    points_per_side=64,
+    points_per_batch=128,
+    pred_iou_thresh=0.7,
+    stability_score_thresh=0.92,
+    stability_score_offset=0.7,
+    crop_n_layers=1,
+    box_nms_thresh=0.7,
+    crop_n_points_downscale_factor=2,
+    min_mask_region_area=25.0,
+    use_m2m=True,
+)
 
 def generate_masked_image(image_array, masks):
     """
